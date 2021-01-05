f7d0db59014884ede1779fd3ee4ee683e33f79e9
Bill <bill@billserver.senders.io>
2021-01-05 17:49:08 -0500

    Tidy

--- docker-run.sh
+++ docker-run.sh
@@ -3,4 +3,4 @@ if [ "$1" == "-d" ]; then
   FLAG="-d"
 fi
 
-docker run $FLAG -v $PWD/www:/usr/share/nginx/html:ro -p 80:80 nginx:latest
+docker run $FLAG -v $PWD/www:/usr/share/nginx/html:ro nginx:latest
